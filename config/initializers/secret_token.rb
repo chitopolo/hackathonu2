89432d01b96260ad4972a33cdc61b20edc2d8433
Rene Polo <renejpolo@me.com>
2013-03-16 13:10:16 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackathonu::Application.config.secret_token = '27cb2a4cf31c6b6bbc96513e9a285ff864121fb870938bcfbe088d63cef81198291f53a3bde81d9c7953d34db4b44ea7f69b0ce9d27244867e3787ac61e2dece'
---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+root = "/home/chitopolo/apps/hackathonu/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.hackathonu.sock"
+worker_processes 2
+timeout 30
